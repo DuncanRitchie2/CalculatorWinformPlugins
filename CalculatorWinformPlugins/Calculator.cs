b3a4349dde342525ddb96052e885ae752d4eaa1a
DuncanRitchie <duncanritchie@btinternet.com>
2019-11-26 13:34:59 +0000

    Added display & num buttons & n1 method

---
+++ CalculatorWinformPlugins/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorWinformPlugins
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void n1_Click(object sender, EventArgs e)
+        {
+            String x = "1";
+            display.Text = display.Text == "0" || display.Text == null ? x : display.Text + x;
+        }
+    }
+}
